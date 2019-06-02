2be310c370fa66a801ff0cdc2e5ac465ea962782
Davi Dias <davidiasdesousa@gmail.com>
2019-06-02 19:13:33 -0300

    Todo App Without redux

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós"></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni voluptatem dignissimos, aliquid inventore natus, asperiores velit nisi consequuntur sed maiores eligendi necessitatibus itaque molestias soluta magnam cupiditate quasi aliquam nihil.</p>
+        <br />
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi aliquam sed libero cumque exercitationem est saepe officiis sint perspiciatis quia, iste optio facilis, suscipit similique, omnis non repellendus quas. Quasi.</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas maiores assumenda, eius nostrum amet unde numquam, aliquam suscipit voluptatibus minus minima officiis aliquid illo! Totam inventore nisi dignissimos similique placeat!</p>
+    </div>
+)
---
+++ frontend/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/app'
+// console.log(App);
+
+
+ReactDOM.render(<App />, document.getElementById('app'))
